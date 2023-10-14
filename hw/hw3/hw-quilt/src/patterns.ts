54fb8e84c20bfe97c9b356d636d6f9aa2cac8d5c
Robert Tang <robertxt@cs.washington.edu>
2023-10-14 16:55:33 -0700

    Implemented Pattern C, D and E.

--- hw/hw3/hw-quilt/src/patterns.ts
+++ hw/hw3/hw-quilt/src/patterns.ts
@@ -1,39 +1,47 @@
-import {Quilt, qnil, ROUND, STRAIGHT, Square, NW, SE, rcons, rnil, qcons} from "./quilt";
+import {Quilt, qnil, ROUND, STRAIGHT, Square, NW, NE, SW, SE, rcons, rnil, qcons} from "./quilt";
 
 './quilt';
 
 
+const squareNERound: Square = { shape: ROUND, color: "GREEN", corner: NE };
+const squareNWRound: Square = { shape: ROUND, color: "GREEN", corner: NW };
+const squareSWRound: Square = { shape: ROUND, color: "GREEN", corner: SW };
+const squareSERound: Square = { shape: ROUND, color: "GREEN", corner: SE };
 
+const squareNWStraight: Square = { shape: STRAIGHT, color: "GREEN", corner: NW };
+// const squareNEStraight: Square = { shape: STRAIGHT, color: "GREEN", corner: NE };
+const squareSEStraight: Square = { shape: STRAIGHT, color: "GREEN", corner: SE };
+// const squareSWStraight: Square = { shape: STRAIGHT, color: "GREEN", corner: SW };
 
 /** Returns a quilt in pattern "A". */
 export const PatternA = (): Quilt => {
-  // TODO: replace
-  const squareNWRound: Square = { shape: ROUND, color: "GREEN", corner: NW };
   const row = rcons(squareNWRound, rcons(squareNWRound, rnil));
   return qcons(row, qcons(row, qcons(row, qcons(row, qnil))));
 }
 
 /** Returns a quilt in pattern "B". */
 export const PatternB = (): Quilt => {
-  // TODO: replace
-  const squareNWStraight: Square = { shape: STRAIGHT, color: "GREEN", corner: NW };
-  const squareSEStraight: Square = { shape: STRAIGHT, color: "GREEN", corner: SE };
-
   const row = rcons(squareSEStraight, rcons(squareNWStraight, rnil));
   return qcons(row, qcons(row, qcons(row, qcons(row, qnil))));
 }
 
 /** Returns a quilt in pattern "C". */
 export const PatternC = (): Quilt => {
-  return qnil;  // TODO: replace
+  const row1 = rcons(squareNERound, rcons(squareNWRound, rnil));
+  const row2 = rcons(squareSERound, rcons(squareSWRound, rnil));
+  return qcons(row1, qcons(row2, qcons(row1, qcons(row2, qnil))));
 }
 
 /** Returns a quilt in pattern "D". */
 export const PatternD = (): Quilt => {
-  return qnil;  // TODO: replace
+  const row1 = rcons(squareSERound, rcons(squareSWRound, rnil));
+  const row2 = rcons(squareNERound, rcons(squareNWRound, rnil));
+  return qcons(row1, qcons(row2, qcons(row1, qcons(row2, qnil))));
 }
 
 /** Returns a quilt in pattern "E". */
 export const PatternE = (): Quilt => {
-  return qnil;  // TODO: replace
+  const row1 = rcons(squareNWStraight, rcons(squareSEStraight, rnil));
+  const row2 = rcons(squareSEStraight, rcons(squareNWStraight, rnil));
+  return qcons(row1, qcons(row2, qcons(row1, qcons(row2, qnil))));
 }
