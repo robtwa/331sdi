bbd57fa88d1ca57dc740bde02b7b5d15cb727ee1
Robert Tang <robertxt@cs.washington.edu>
2023-10-28 17:46:23 -0700

    hw5: Q1.a-c on color_list.ts

--- hw/hw5/hw-highlight/src/color_list.ts
+++ hw/hw5/hw-highlight/src/color_list.ts
@@ -2,15 +2,52 @@ import { ColorInfo, COLORS } from './colors';
 import { List, cons, nil } from './list';
 
 // TODO: add interfaces and classes here
+/** Represents a color list */
+export interface ColorList {
+  /**
+   * Returns a list of all color names that include the given text
+   * @param text Text to look for in the names of the colors (case insensitive)
+   * @returns list of all color names that include the given text
+   */
+  findMatchingNames: (text: string) => List<string>;
 
+  /**
+   * Returns the background and foreground CSS colors to highlight with this color.
+   * @param name Name of the color to look for
+   * @throws Error if there is no such color
+   * @returns (bg, fg) where bg is the CSS background color and fg is foreground
+   */
+  getColorCss: (name: string) => readonly [string, string];
+}
+
+// Implementation of the color list
+class SimpleColorList implements ColorList {
+  // AF: obj = this.colors
+  readonly colors: List<ColorInfo>;
+
+  // Creates a instance with the given colors.
+  // make obj = colors
+  constructor(colors: List<ColorInfo>) {
+    this.colors = colors;
+  }
+
+  findMatchingNames = (text: string): List<string> => {
+    return findMatchingNamesIn(text, this.colors);
+  };
+
+  getColorCss = (name: string): readonly [string, string] => {
+    return getColorCssIn(name,  this.colors);
+  };
+}
+
+// Creates an instance of SimpleColorList with the “singleton” pattern
+const instanceOfSCList: SimpleColorList = new SimpleColorList(COLORS);
 /**
- * Returns a list of all color names that include the given text
- * @param text Text to look for in the names of the colors (case insensitive)
- * @returns list of all color names that include the given text
+ * Returns the same instance of SimpleColorList.
  */
-export const findMatchingNames = (text: string): List<string> => {
-  return findMatchingNamesIn(text, COLORS);
-};
+export const makeSimpleColorList = (): SimpleColorList => {
+  return instanceOfSCList;
+}
 
 // Returns a new list containing just the names of those colors that include the
 // given text.
@@ -23,7 +60,7 @@ const findMatchingNamesIn =
   if (colors === nil) {
     return nil;
   } else {
-    // Note: the _ prevents the typechecker froom complaining about  
+    // Note: the _ prevents the typechecker froom complaining about
     // our defining these variables and not using them which we must
     // do to avoid tuple indexing
     const [color, _css, _foreground] = colors.hd;
@@ -35,16 +72,6 @@ const findMatchingNamesIn =
   }
 };
 
-
-/**
- * Returns the background and foreground CSS colors to highlight with this color.
- * @param name Name of the color to look for
- * @throws Error if there is no such color
- * @returns (bg, fg) where bg is the CSS background color and fg is foreground
- */
-export const getColorCss = (name: string): readonly [string, string] => {
-  return getColorCssIn(name, COLORS);
-};
 
 // Returns the colors from the (first) list entry with this color name. Throws
 // an Error none is found (i.e., we hit the end of the list).
