18ae9e5b6e2a20abf97f477364f5ba3484e4b3e5
Robert Tang <robertxt@cs.washington.edu>
2023-10-30 19:35:19 -0700

    hw5: Q5 (d) Write test cases for findHighlights in the file parse_test.ts.

--- hw/hw5/hw-highlight/src/parser_test.ts
+++ hw/hw5/hw-highlight/src/parser_test.ts
@@ -1,7 +1,12 @@
 import * as assert from 'assert';
-import { explode } from './char_list';
-import { explode_array } from './list';
-import { getNextHighlight, parseHighlightLines } from './parser';
+import { explode} from './char_list';
+import {explode_array, nil} from './list';
+import {
+  findHighlights,
+  getNextHighlight,
+  parseHighlightLines,
+  parseHighlightText
+} from './parser';
 
 
 describe('parser', function() {
@@ -49,22 +54,115 @@ describe('parser', function() {
         ["my ", {color: "red", text: "ball"}, explode(" is great")]);
     assert.deepStrictEqual(getNextHighlight(explode("grass is [green|itchy]")),
         ["grass is ", {color: "green", text: "itchy"}, explode("")]);
+
+    console.log(getNextHighlight(explode("[red|ball]")));
   });
 
   it('findHighlights', function() {
     // TODO: Add tests here
+    // 0-1-many: base case
+    assert.deepEqual(findHighlights(explode("")), nil);
+    assert.deepEqual(findHighlights(explode("my book")),
+      explode_array([{color: 'white', text: 'my book'}]));
+
+    // 0-1-many: 1 recursive call (1st)
+    assert.deepEqual(
+      findHighlights(explode("my [green|green] book")),
+      explode_array([
+        {color: 'white', text: 'my '},
+        {color: 'green', text: 'green'},
+        {color: 'white', text: ' book'},
+      ]));
+    // 0-1-many: 1 recursive call (2nd)
+    assert.deepEqual(
+      findHighlights(explode("[green|green] book")),
+      explode_array([
+        {color: 'green', text: 'green'},
+        {color: 'white', text: ' book'},
+      ]));
+    // 0-1-many: 1 recursive call (3rd)
+    assert.deepEqual(
+      findHighlights(explode("[green|green]")),
+      explode_array([
+        {color: 'green', text: 'green'},
+      ]));
+
+    // 0-1-many: more than 1 recursive call (1st)
+    assert.deepEqual(
+      findHighlights(explode("my [green|green] [blue|story] book")),
+      explode_array([
+        {color: 'white', text: 'my '},
+        {color: 'green', text: 'green'},
+        {color: 'white', text: ' '},
+        {color: 'blue', text: 'story'},
+        {color: 'white', text: ' book'},
+      ]));
+    // 0-1-many: more than 1 recursive call (2nd)
+    assert.deepEqual(
+      findHighlights(explode("[green|green] [blue|story] book")),
+      explode_array([
+        {color: 'green', text: 'green'},
+        {color: 'white', text: ' '},
+        {color: 'blue', text: 'story'},
+        {color: 'white', text: ' book'},
+      ]));
+    // 0-1-many: more than 1 recursive call (3rd)
+    assert.deepEqual(
+      findHighlights(explode("my [green|green] [blue|story]")),
+      explode_array([
+        {color: 'white', text: 'my '},
+        {color: 'green', text: 'green'},
+        {color: 'white', text: ' '},
+        {color: 'blue', text: 'story'},
+      ]));
+    // 0-1-many: more than 1 recursive call (4th)
+    assert.deepEqual(
+      findHighlights(explode("[green|green] [blue|story]")),
+      explode_array([
+        {color: 'green', text: 'green'},
+        {color: 'white', text: ' '},
+        {color: 'blue', text: 'story'},
+      ]));
+    // 0-1-many: more than 1 recursive call (5th)
+    assert.deepEqual(
+      findHighlights(explode("[green|green] [blue|story] [red|book]")),
+      explode_array([
+        {color: 'green', text: 'green'},
+        {color: 'white', text: ' '},
+        {color: 'blue', text: 'story'},
+        {color: 'white', text: ' '},
+        {color: 'red', text: 'book'},
+      ]));
   });
 
   // TODO: Uncomment to test
-//  it('parseHighlightText', function() {
-//    assert.deepEqual(parseHighlightText(""), explode_array([]));
-//    assert.deepEqual(
-//      parseHighlightText("my [red|favorite] book"),
-//      explode_array([
-//        {color: 'white', text: 'my '},
-//        {color: 'red', text: 'favorite'},
-//        {color: 'white', text: ' book'},
-//      ]));
-//  });
+ it('parseHighlightText', function() {
+   assert.deepEqual(parseHighlightText(""), explode_array([]));
+
+   assert.deepEqual(parseHighlightText("my book"), explode_array([{color: 'white', text: 'my book'}]));
+
+   assert.deepEqual(
+     parseHighlightText("my [red|favorite] book"),
+     explode_array([
+       {color: 'white', text: 'my '},
+       {color: 'red', text: 'favorite'},
+       {color: 'white', text: ' book'},
+     ]));
+
+   assert.deepEqual(
+     parseHighlightText("[red|one][blue|two]"),
+     explode_array([
+       {color: 'red', text: 'one'},
+       {color: 'blue', text: 'two'},
+     ]));
+
+   assert.deepEqual(
+     parseHighlightText("[red|one][blue|two][green|three]"),
+     explode_array([
+       {color: 'red', text: 'one'},
+       {color: 'blue', text: 'two'},
+       {color: 'green', text: 'three'},
+     ]));
+ });
 
 });
