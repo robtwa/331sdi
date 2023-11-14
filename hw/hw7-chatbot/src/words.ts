7b32a73ed3418861e4375cafc41450d9d8d70e1e
Robert Tang <robertxt@cs.washington.edu>
2023-11-13 19:26:48 -0800

    hw7: q6 - fix lint issues for words.ts

--- hw/hw7-chatbot/src/words.ts
+++ hw/hw7-chatbot/src/words.ts
@@ -40,41 +40,41 @@ export const substitute = (words: string[], reps: Map<string, string>): void => 
  *           join(L @ [S @ [w]]) = join(L @ S) @ [w]
  */
 export const replaceWords =
-    (words: ReadonlyArray<string>,
-     replacements: Map<string, ReadonlyArray<string>>): string[] => {
+  (words: ReadonlyArray<string>,
+   replacements: Map<string, ReadonlyArray<string>>): string[] => {
 
-  const replaced: ReadonlyArray<string>[] = [];
-  let i = 0;
+    const replaced: ReadonlyArray<string>[] = [];
+    let i = 0;
 
-  // Inv: replaced[0..i-1] = map(words[0..i-1], replacements) and
-  //      replaced[i..n-1] is unchanged
-  while (i !== words.length) {
-    const val = replacements.get(words[i]);
-    if (val !== undefined) {
-      replaced.push(val);
-    } else {
-      replaced.push([words[i]]);
+    // Inv: replaced[0..i-1] = map(words[0..i-1], replacements) and
+    //      replaced[i..n-1] is unchanged
+    while (i !== words.length) {
+      const val = replacements.get(words[i]);
+      if (val !== undefined) {
+        replaced.push(val);
+      } else {
+        replaced.push([words[i]]);
+      }
+      i = i + 1;
     }
-    i = i + 1;
-  }
 
-  const result: string[] = [];
-  let j = 0;
+    const result: string[] = [];
+    let j = 0;
 
-  // Inv: result = join(replaced[0..j-1])
-  while (j !== replaced.length) {
-    const L = replaced[j];
-    let k = 0;
-    // Inv: result = join(replaced[0..j-1]) @ L[0..k-1]
-    while (k !== L.length) {
-      result.push(L[k])
-      k = k + 1;
+    // Inv: result = join(replaced[0..j-1])
+    while (j !== replaced.length) {
+      const L = replaced[j];
+      let k = 0;
+      // Inv: result = join(replaced[0..j-1]) @ L[0..k-1]
+      while (k !== L.length) {
+        result.push(L[k])
+        k = k + 1;
+      }
+      j = j + 1;
     }
-    j = j + 1;
-  }
 
-  return result;
-};
+    return result;
+  };
 
 // String containing all punctuation characters.
 const PUNCT: string = ",.?;:!";
@@ -102,26 +102,24 @@ const isPunct = (ch: string): boolean => {
 export const splitWords = (str: string): string[] => {
   let splits: number[] = [0];  // (part a): fix this
   let j: number = 0;           // (part a): fix this
-  let last:number|undefined;   // the last element in the splits array
 
   CheckInv1(splits, str, j);
 
+
   // Inv: 1. 0 = splits[0] < splits[1] < ... < splits[n-1] = j
-  //      2. for i = 0 .. n-1, if splits[i+1] - splits[i] > 1, then 
-  //                      str[splits[i] ..  splits[i+1]-1] is all letters
+  //      2. for i = 0 .. n-1, if splits[i+1] - splits[i] > 1, then
+  //         str[splits[i] ..  splits[i+1]-1] is all letters
   //      3. for i = 1 .. n-1, splits[i] is not between two letters
-  //                      where n = splits.length
-  while (j !== str.length) {  // (part 5a): fix this
+  //  where n = splits.length
+  while (j !== str.length) {
     // (part 5a): implement this
-    if (j < 1
-      || (str[j] === " " || isPunct(str[j]))
-      || (j > 0 && (str[j-1] === " " || isPunct(str[j-1])))) {
+    if (j < 1 || (str[j] === " " || isPunct(str[j])) || (j > 0 && (str[j - 1] === " " || isPunct(str[j - 1])))) {
       splits.push(j + 1);
-    }
-    else {
-      last = splits.pop();
-      if(last !== undefined) {
-        splits.push(<number>last + 1 );
+    } else {
+      // the last element in the "splits" array
+      let last: number | undefined = splits.pop();
+      if (last !== undefined) {
+        splits.push(last + 1);
       }
     }
 
@@ -136,16 +134,16 @@ export const splitWords = (str: string): string[] => {
 
   // Inv: 1. join(words) = del-space(s[0..splits[i]-1]))
   //      2. no element of words contains any whitespace
-  while (i+1 !== splits.length) {
+  while (i + 1 !== splits.length) {
     if (str[splits[i]] !== " ")
-      words.push(str.substring(splits[i], splits[i+1]));
+      words.push(str.substring(splits[i], splits[i + 1]));
     i = i + 1;
     CheckInv2(words, splits, str, i);
   }
 
   // Post: join(words) = del-space(str), each punctuation is its own word,
   //       adjacent letters are in the same word, and no word has spaces
-  return words; 
+  return words;
 };
 
 // Verify that the invariant from the first loop of splitWords holds.
@@ -155,26 +153,26 @@ const CheckInv1 = (splits: number[], str: string, j: number): void => {
 
   if (splits.length === 0 || splits[0] !== 0)
     throw new Error('splits should start with 0');
-  if (splits[splits.length-1] !== j)
+  if (splits[splits.length - 1] !== j)
     throw new Error(`splits should end with the string's length ${j}`);
 
   const n = splits.length - 1;
   // Inv: checked the invariant for splits[0 .. i-1]
   for (let i = 0; i < n; i++) {
-    if (splits[i+1] - splits[i] <= 0)
-      throw new Error(`should have at least 1 char between splits at ${splits[i]} and ${splits[i+1]}`);
+    if (splits[i + 1] - splits[i] <= 0)
+      throw new Error(`should have at least 1 char between splits at ${splits[i]} and ${splits[i + 1]}`);
 
-    const w = str.substring(splits[i], splits[i+1]);
+    const w = str.substring(splits[i], splits[i + 1]);
     if (w.length > 1) {
       // Inv: w[0 .. j-1] is all letters
       for (let j = 0; j < w.length; j++) {
         if (w[j] === " " || isPunct(w[j]))
           throw new Error(`space/punct "${w[j]}" is in a part with other characters`);
       }
-    } 
+    }
 
     if (i > 0) {
-      const c1 = str[splits[i]-1];
+      const c1 = str[splits[i] - 1];
       const c2 = str[splits[i]];
       if ((c1 !== " ") && !isPunct(c1) && (c2 !== " ") && !isPunct(c2))
         throw new Error(`split at ${splits[i]} is between two letters "${c1}" and "${c2}"`);
@@ -184,22 +182,22 @@ const CheckInv1 = (splits: number[], str: string, j: number): void => {
 
 // Verify that the invariant from the second loop of splitWords holds.
 const CheckInv2 =
-    (words: string[], splits: number[], str: string, i: number): void => {
-  if (!DEBUG)
-    return;  // skip this
+  (words: string[], splits: number[], str: string, i: number): void => {
+    if (!DEBUG)
+      return;  // skip this
 
-  const s1 = words.join("");
-  if (s1.indexOf(" ") >= 0)
-    throw new Error(`words contains space charactrs: "${s1}"`);
+    const s1 = words.join("");
+    if (s1.indexOf(" ") >= 0)
+      throw new Error(`words contains space charactrs: "${s1}"`);
 
-  let s2 = str.slice(0, splits[i]);
-  // Inv: s2 = str[0..splits[i]-1] with some spaces removed
-  while (s2.indexOf(" ") >= 0)
-    s2 = s2.replace(" ", "");
+    let s2 = str.slice(0, splits[i]);
+    // Inv: s2 = str[0..splits[i]-1] with some spaces removed
+    while (s2.indexOf(" ") >= 0)
+      s2 = s2.replace(" ", "");
 
-  if (s1 !== s2)
-    throw new Error(`words do not match the string (minus spaces): "${s1}" vs "${s2}"`);
-};
+    if (s1 !== s2)
+      throw new Error(`words do not match the string (minus spaces): "${s1}" vs "${s2}"`);
+  };
 
 
 /**
@@ -211,44 +209,44 @@ const CheckInv2 =
  *     or -1 if none exists
  */
 export const wordsContain =
-    (all: ReadonlyArray<string>, sub: ReadonlyArray<string>): number => {
+  (all: ReadonlyArray<string>, sub: ReadonlyArray<string>): number => {
 
-  if (sub.length === 0)
-    throw new Error("second list of words cannot be empty");
-  if (all.length < sub.length)
-    return -1; // not enough words to contain sub
+    if (sub.length === 0)
+      throw new Error("second list of words cannot be empty");
+    if (all.length < sub.length)
+      return -1; // not enough words to contain sub
 
-  let k: number = -1;
+    let k: number = -1;
 
-  // Inv: no index 0 <= j <= k such that
-  //      lower(all[j+i]) = lower(sub[i]) for i = 0 .. sub.length-1
-  while (k + sub.length !== all.length) {
-    k = k + 1;
+    // Inv: no index 0 <= j <= k such that
+    //      lower(all[j+i]) = lower(sub[i]) for i = 0 .. sub.length-1
+    while (k + sub.length !== all.length) {
+      k = k + 1;
 
-    let m: number = 0;
+      let m: number = 0;
 
-    // Inv: outer Inv and lower(all[k+i]) = lower(sub[i]) for i = 0 .. m-1
-    while (m !== sub.length && all[k+m].toLowerCase() === sub[m].toLowerCase()) {
-      m = m + 1;
+      // Inv: outer Inv and lower(all[k+i]) = lower(sub[i]) for i = 0 .. m-1
+      while (m !== sub.length && all[k + m].toLowerCase() === sub[m].toLowerCase()) {
+        m = m + 1;
+      }
+
+      if (m === sub.length) {
+        // all[k+i] = sub[i] for i = 0 .. sub.length-1
+        return k;  // j = k matches
+      }
     }
 
-    if (m === sub.length) {
-      // all[k+i] = sub[i] for i = 0 .. sub.length-1
-      return k;  // j = k matches
-    }
-  }
-
-  // Post: no index 0 <= j <= all.length - sub.length such that
-  //       all[j+i] = sub[i] for i = 0 .. sub.length-1
-  return -1;
-};
+    // Post: no index 0 <= j <= all.length - sub.length such that
+    //       all[j+i] = sub[i] for i = 0 .. sub.length-1
+    return -1;
+  };
 
 
 /**
  * Returns a string containing all of the given words, in the same order, but
  * with spaces before each (non-punctuation) word other than the first.
  * @param words list of words (no spaces, punctuation as its own words)
- * @return join-words(words), where 
+ * @return join-words(words), where
  *     join-words([]) = ""
  *     join-words([w]) = w
  *     join-words(L @ [v, w]) =
@@ -257,21 +255,20 @@ export const wordsContain =
  */
 export const joinWords = (words: ReadonlyArray<string>): string => {
   // (part 4a): handle the case when the array is empty
-  if (words.length === 0 ) {
+  if (words.length === 0) {
     return "";
   }
 
   // (part 4b): write a loop for the case when the array is not empty
-  let j:number = 0;         // an index into the array
-  let parts:string[] = [];  // an array of strings
+  let j: number = 0;         // an index into the array
+  let parts: string[] = [];  // an array of strings
 
-  //{{Inv: join(parts) = join-words(words[0 .. j − 1]) }}
+  // Inv: join(parts) = join-words(words[0 .. j − 1])
   while (j < words.length) {
     // If w is the first element in the array or w is a punctuation mark
-    if ( j < 1 || (words[j].length === 1 && isPunct(words[j]))) {
+    if (j < 1 || (words[j].length === 1 && isPunct(words[j]))) {
       parts[j] = words[j];
-    }
-    else {  // otherwise
+    } else {  // otherwise
       parts[j] = " " + words[j];
     }
     j = j + 1;
