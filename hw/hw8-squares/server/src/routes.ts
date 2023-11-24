005029d383349b45a8e974c8847c7378fa2a4a76
Robert Tang <robertxt@cs.washington.edu>
2023-11-24 12:58:27 -0800

    hw8: backend - document the routes.ts

--- hw/hw8-squares/server/src/routes.ts
+++ hw/hw8-squares/server/src/routes.ts
@@ -6,75 +6,42 @@ import { ParamsDictionary } from "express-serve-static-core";
 type SafeRequest = Request<ParamsDictionary, {}, Record<string, unknown>>;
 type SafeResponse = Response;  // only writing, so no need to check
 
-// files
+// Storing all files with the map data structure
 const files: Map<string, object> = new Map();
 
-/** Returns a list of all the named save files. */
-export const list = (req: SafeRequest, res: SafeResponse): void => {
-  console.log("list")
-  console.log(req.query)
-
-  const data = Array.from(files.keys());
-  console.log(data)
-  res.send(JSON.stringify(data));
-};
-
-export const load = (req: SafeRequest, res: SafeResponse): void => {
-  console.log("load ////////////////// ")
-  const name = first(req.query.name);
-  console.log("name = " + name)
-
-  if (name === undefined) {
-    res.status(400).send('missing "name" parameter');
-    return;
-  }
-  const data = files.get(name);
-  console.log("data = ", data)
-  res.send(JSON.stringify(data));
-};
-
-export const deleteSq = (req: SafeRequest, res: SafeResponse): void => {
-  console.log("deleteSq ////////////////// ")
-  const name = first(req.query.name);
-  console.log("name = " + name)
-
-  if (name === undefined) {
-    res.status(400).send('missing "name" parameter');
-    return;
-  }
-
-  files.delete(name);
-  const data = Array.from(files.keys());
-  console.log(data)
-  res.send(JSON.stringify(data));
-};
-
+/**
+ * Returns the saved square file name.
+ * If the request does not specify a file name, a 400 error is returned and
+ * contains error information.
+ * If the file name specified in the request does not exist, a 400 error is
+ * returned and contains error information.
+ * If no square data is provided or the data is empty, a 400 error and related
+ * error information will be returned.
+ *
+ * @param req The HTTP request object
+ * @param res The HTTP response object
+ */
 export const save = (req: SafeRequest, res: SafeResponse): void => {
-  console.log("save //////////////////////////")
-  const name = req.body?.name;
+  const filename = first(req.body.filename);
   const data = req.body?.data;
-  console.log("data = ", data)
 
   // validation
-  if (name === undefined) {
+  if (filename === undefined) {
     // http code: 400 -> bad request
-    res.status(400).send('missing "name" parameter');
+    res.status(400).send('missing "filename" parameter');
     return;
   }
-  if (name === "") {
-    res.status(400).send('The "name" parameter must be at least one character');
+  else if (filename === "") {
+    res.status(400).send('The "filename" parameter must be at least ' +
+      'one character');
     return;
   }
-  else if (typeof name !== "string") {
-    res.status(400).send('The "name" parameter must be a string');
-    return;
-  }
-  else if (data === undefined || data === null) {
+  else if (data === undefined) {
     res.status(400).send('missing "data" parameter');
     return;
   }
-  else if (typeof name !== "string") {
-    res.status(400).send('The "name" parameter must be a string');
+  else if (data === null) {
+    res.status(400).send('"data" cannot be null');
     return;
   }
   else {
@@ -83,16 +50,78 @@ export const save = (req: SafeRequest, res: SafeResponse): void => {
     // "The lack of type information should not be a problem as it should not be
     // necessary to examine the contents of the file. We simply store it in
     // save, and return it in load."
-    files.set(name, data);  // add to files
-    console.log("files = ", files)
-    res.send({res: `${name}`});
+    files.set(filename, data);  // add to files
+    res.send({res: `${filename}`});
   }
 };
 
 
+/**
+ * Returns a JSON String that is an array containing the names of all saved
+ * files. If there are no saved files, an empty array is returned in JSON string
+ * format.
+ *
+ * @param _ The HTTP request object
+ * @param res The HTTP response object
+ */
+export const list = (_: SafeRequest, res: SafeResponse): void => {
+  const data = Array.from(files.keys());
+  res.send(JSON.stringify(data));
+};
+
+/**
+ * Returns a JSON string containing the square data for the requested file.
+ * If the request does not specify a file name, a 400 error is returned and
+ * contains error information.
+ * If the file name specified in the request does not exist, a 400 error is
+ * returned and contains error information.
+ *
+ * @param req The HTTP request object
+ * @param res The HTTP response object
+ */
+export const load = (req: SafeRequest, res: SafeResponse): void => {
+  const name = first(req.query.name);
+  if (name === undefined) {
+    res.status(400).send('missing "name" parameter');
+    return;
+  }
+  else if (!files.has(name)) {
+    res.status(400).send('There is no file with this name');
+    return;
+  }
+  const data = files.get(name);
+  res.send(JSON.stringify(data));
+};
+
+/**
+ * Delete a specified square file and return a file list that does not contain
+ * the deleted file.
+ * If the request does not specify a file name, a 400 error is returned and
+ * contains error information.
+ * If the file name specified in the request does not exist, a 400 error is
+ * returned and contains error information.
+ *
+ * @param req The HTTP request object
+ * @param res The HTTP response object
+ */
+export const remove = (req: SafeRequest, res: SafeResponse): void => {
+  const name = first(req.query.name);
+  if (name === undefined) {
+    res.status(400).send('missing "name" parameter');
+    return;
+  }
+  else if (!files.has(name)) {
+    res.status(400).send('There is no file with this name');
+    return;
+  }
+
+  files.delete(name);  // delete file
+  const data = Array.from(files.keys());  // generate a new file list
+  res.send(JSON.stringify(data));  // send
+};
+
+
 // Helper functions ***********************************************************
-
-
 
 // Helper to return the (first) value of the parameter if any was given.
 // (This is mildly annoying because the client can also give mutiple values,
@@ -106,3 +135,9 @@ const first = (param: unknown): string|undefined => {
     return undefined;
   }
 };
+
+// Helper at the end of each test to call this function to delete any
+// all files saved.
+export const resetForTesting  = ():void => {
+  files.clear();
+}
