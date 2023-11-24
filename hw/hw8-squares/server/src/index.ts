8c2462c6e2ace081489a1568a23cecec42686ca0
Robert Tang <robertxt@cs.washington.edu>
2023-11-23 16:04:34 -0800

    hw8: frontend - index.tsx

--- hw/hw8-squares/server/src/index.ts
+++ hw/hw8-squares/server/src/index.ts
@@ -1,5 +1,5 @@
 import express, { Express } from "express";
-import { dummy } from './routes';
+import { save, list, load, deleteSq } from './routes';
 import bodyParser from 'body-parser';
 
 
@@ -7,5 +7,10 @@ import bodyParser from 'body-parser';
 const port: number = 8088;
 const app: Express = express();
 app.use(bodyParser.json());
-app.get("/api/dummy", dummy);
+app.post("/api/save", save);
+app.get("/api/list", list);
+app.get("/api/load", load);
+app.get("/api/delete", deleteSq);
+
+
 app.listen(port, () => console.log(`Server listening on ${port}`));
