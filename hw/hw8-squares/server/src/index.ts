2acc953e7a0176271c069112ca9f6e617ea01634
Robert Tang <robertxt@cs.washington.edu>
2023-11-24 14:41:35 -0800

    hw8: backend - rename the function for removing file in index.ts

--- hw/hw8-squares/server/src/index.ts
+++ hw/hw8-squares/server/src/index.ts
@@ -1,5 +1,5 @@
 import express, { Express } from "express";
-import { save, list, load, deleteSq } from './routes';
+import {save, list, load, remove} from './routes';
 import bodyParser from 'body-parser';
 
 
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.post("/api/save", save);
 app.get("/api/list", list);
 app.get("/api/load", load);
-app.get("/api/delete", deleteSq);
+app.get("/api/delete", remove);
 
 
 app.listen(port, () => console.log(`Server listening on ${port}`));
