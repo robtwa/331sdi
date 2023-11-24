d4d13f9c6b5a4b3924e5e65f02d485c615f98e58
Robert Tang <robertxt@cs.washington.edu>
2023-11-23 16:04:45 -0800

    hw8: frontend - routes.tsx

--- hw/hw8-squares/server/src/routes.ts
+++ hw/hw8-squares/server/src/routes.ts
@@ -6,17 +6,92 @@ import { ParamsDictionary } from "express-serve-static-core";
 type SafeRequest = Request<ParamsDictionary, {}, Record<string, unknown>>;
 type SafeResponse = Response;  // only writing, so no need to check
 
+// files
+const files: Map<string, object> = new Map();
 
 /** Returns a list of all the named save files. */
-export const dummy = (req: SafeRequest, res: SafeResponse): void => {
+export const list = (req: SafeRequest, res: SafeResponse): void => {
+  console.log("list")
+  console.log(req.query)
+
+  const data = Array.from(files.keys());
+  console.log(data)
+  res.send(JSON.stringify(data));
+};
+
+export const load = (req: SafeRequest, res: SafeResponse): void => {
+  console.log("load ////////////////// ")
   const name = first(req.query.name);
+  console.log("name = " + name)
+
+  if (name === undefined) {
+    res.status(400).send('missing "name" parameter');
+    return;
+  }
+  const data = files.get(name);
+  console.log("data = ", data)
+  res.send(JSON.stringify(data));
+};
+
+export const deleteSq = (req: SafeRequest, res: SafeResponse): void => {
+  console.log("deleteSq ////////////////// ")
+  const name = first(req.query.name);
+  console.log("name = " + name)
+
   if (name === undefined) {
     res.status(400).send('missing "name" parameter');
     return;
   }
 
-  res.send({greeting: `Hi, ${name}`});
+  files.delete(name);
+  const data = Array.from(files.keys());
+  console.log(data)
+  res.send(JSON.stringify(data));
 };
+
+export const save = (req: SafeRequest, res: SafeResponse): void => {
+  console.log("save //////////////////////////")
+  const name = req.body?.name;
+  const data = req.body?.data;
+  console.log("data = ", data)
+
+  // validation
+  if (name === undefined) {
+    // http code: 400 -> bad request
+    res.status(400).send('missing "name" parameter');
+    return;
+  }
+  if (name === "") {
+    res.status(400).send('The "name" parameter must be at least one character');
+    return;
+  }
+  else if (typeof name !== "string") {
+    res.status(400).send('The "name" parameter must be a string');
+    return;
+  }
+  else if (data === undefined || data === null) {
+    res.status(400).send('missing "data" parameter');
+    return;
+  }
+  else if (typeof name !== "string") {
+    res.status(400).send('The "name" parameter must be a string');
+    return;
+  }
+  else {
+    // Write to file ////////////////////////////////////////////////////
+    // I do not do type checking of the data because the specs said:
+    // "The lack of type information should not be a problem as it should not be
+    // necessary to examine the contents of the file. We simply store it in
+    // save, and return it in load."
+    files.set(name, data);  // add to files
+    console.log("files = ", files)
+    res.send({res: `${name}`});
+  }
+};
+
+
+// Helper functions ***********************************************************
+
 
 
 // Helper to return the (first) value of the parameter if any was given.
