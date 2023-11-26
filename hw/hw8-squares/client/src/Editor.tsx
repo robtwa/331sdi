4c2083c97733f005f26e6dbc1b591c8c4e600d7a
Robert Tang <robertxt@cs.washington.edu>
2023-11-26 14:44:03 -0800

    hw8: changes made to Editor.tsx

--- hw/hw8-squares/client/src/Editor.tsx
+++ hw/hw8-squares/client/src/Editor.tsx
@@ -6,10 +6,12 @@ import {
   toColor,
   Color,
   toJson,
-  fromJson, dirToIdx
+  fromJson,
+  dirToIdx
 } from './square';
 import { SquareElem } from "./square_draw";
 import {nil} from "./list";
+import './index.css';
 
 type EditorProps = {
   /** Initial state of the file. */
@@ -29,7 +31,6 @@ type EditorState = {
 
   color?: Color;
 };
-
 
 /** UI for editing the image. */
 export class Editor extends Component<EditorProps, EditorState> {
@@ -61,8 +62,10 @@ export class Editor extends Component<EditorProps, EditorState> {
   // Returns the UI of the tool buttons
   tools = (): JSX.Element => {
     return <div className="buttonArea">
-      <button id="btn_split" className="button" onClick={this.doSplitClick} >Split</button>
-      <button id="btn_merge" className="button" onClick={this.doMergeClick} >Merge</button>
+      <button id="btn_split" className="button"
+              onClick={this.doSplitClick} >Split</button>
+      <button id="btn_merge" className="button"
+              onClick={this.doMergeClick} >Merge</button>
       <select onChange={this.doColorChange} value="NA" className="select">
         <option key={"color_NA"} value="NA">Pick a color</option>
         {Colors.map((color, index)=>(
@@ -78,8 +81,11 @@ export class Editor extends Component<EditorProps, EditorState> {
     this.props.saveFileFunc(this.state.root)
   };
 
-  // Event handler for clicking a square
-  // Update the path of the clicked square to the state object.
+  /**
+   * Event handler for clicking a square
+   * Update the path of the clicked square to the state object.
+   * @param path a list of dir
+   */
   doSquareClick = (path: Path): void => {
     this.setState({selected:path});
   }
@@ -93,27 +99,39 @@ export class Editor extends Component<EditorProps, EditorState> {
   };
 
   /**
-   * Splitting function of the square.
-   * If the path is nil, return 4 squares with the current color
-   * If the path is valid and is not nil and the root is an array of length 4,
-   * insert 4 additional squares on the selected square and return it, otherwise
-   * print an error message to the console
-   *
-   * @param path the path of the target square
+   * Returns the split square tree after splitting the selected square.
+   * If no path is selected, return the root.
+   * If the path is a nil, return 4 squares with the color of the selected
+   *    square.
+   * If the path is not a nil and the root is a valid square array,
+   *    insert 4 additional squares on the selected square and return it,
+   *    otherwise print an error message to the console
+   * @param path the path of the target square, it is undefined if no path
+   *        selected yet
    * @param root the root of the square tree
    */
   splitSq = (path: Path | undefined, root: unknown): unknown => {
-    if (path === nil) {
+    if (path === undefined) { // No path selected yet
+      return root;
+    }
+
+    if (path === nil) {  // base case
       return [root, root, root, root];
     }
-    else {
-      if(Array.isArray(root) && root.length === 4 && path?.hd !== undefined) {
-        let data = [root[0], root[1], root[2], root[3]];
-        data[dirToIdx[path.hd]] = this.splitSq(path.tl, root[dirToIdx[path.hd]]);
+    else {  // recursive case
+      // Check if root is a valid square array
+      if(Array.isArray(root) && root.length === 4) {
+        // if root is a valid square array
+        const data:unknown[] = root;
+        // Split the square at the specified index by converting dir to the
+        // index of the square array
+        data[dirToIdx[path.hd]] = this.splitSq(path.tl,
+                                                  root[dirToIdx[path.hd]]);
         return data;
       }
       else {
-        console.error(`type ${typeof root} is not a valid square`);
+        // root is not a valid square array
+        console.error(`type ${typeof root} is not a valid square array`);
         return root;
       }
     }
@@ -128,33 +146,44 @@ export class Editor extends Component<EditorProps, EditorState> {
   };
 
   /**
-   * Merge function of squares
-   * If the path is nil, return the root.
-   * If the path is valid and is not nil and the root is an array of length 4,
-   * merge other squares with the selected square and return it, otherwise print
-   * an error message to the console
-   * @param path the path of the target square
+   * Returns the merged square tree after merging the selected square with its
+   * siblings.
+   * If the path is undefined, return the root.
+   * If there is only one square, return the root.
+   * If the root is a valid square array, merge other three squares with the
+   *    selected square and return it, otherwise print an error message to the
+   *    console
+   * @param path the path of the target square, undefined for not select a path
    * @param root the root of the square tree
    */
   mergeSq = (path: Path | undefined, root:unknown):unknown =>{
-    if (path === nil) {
+    if (path === undefined) {
+      // if no path selected yet
       return root;
     }
+
+    if (typeof root === "string" || path === nil) {
+      // If there is only one square
+      return root;
+    }
+
+    // Check to see if root is a valid array for square
+    if((Array.isArray(root) && root.length === 4)) {
+      if (path.tl === nil) {  // base case - If the next square is nil
+        return root[dirToIdx[path.hd]];
+      }
+      else {  // recursive cases
+        // Continue to find the root of the selected square according to the
+        // path
+        const data:unknown[] = root;
+        data[dirToIdx[path.hd]] = this.mergeSq(path.tl,
+                                                root[dirToIdx[path.hd]]);
+        return data;
+      }
+    }
     else {
-      if(Array.isArray(root) && root.length === 4 && path?.hd !== undefined) {
-        if (path?.tl === nil) {
-          return root[dirToIdx[path.hd]];
-        }
-        else {
-          let data = [root[0], root[1], root[2], root[3]];
-          data[dirToIdx[path.hd]] = this.mergeSq(path.tl, root[dirToIdx[path.hd]]);
-          return data;
-        }
-      }
-      else {
-        console.error(`type ${typeof root} is not a valid square`);
-        return root;
-      }
+      console.error(`type ${typeof root} is not a valid square`);
+      return root;
     }
   }
 
@@ -164,35 +193,49 @@ export class Editor extends Component<EditorProps, EditorState> {
    */
   doColorChange = (_evt: ChangeEvent<HTMLSelectElement>): void => {
     // Task: implement
-    const color: Color = toColor(_evt.target.value);
-    const path: Path | undefined = this.state.selected;
+    const color: Color = toColor(_evt.target.value);  // get the selected color
+    const path: Path | undefined = this.state.selected;  //
     const tree = toJson(this.state.root);
     const data = this.changeColor(color, path, tree);
     this.setState({root: fromJson(data), color: color})
   };
 
   /**
-   * Function for changing a square's color.
-   * If the path is nil, return the color.
-   * If the path is valid and is not nil and the root is an array of length 4,
-   * change the color the selected square and return it, otherwise print an
-   * error message to the console
-   *
+   * Return the square tree after changing color on the selected square.
+   * If the path is a nil, return the color.
+   * If the path is not a nil and the root is a valid square array,
+   *    change the color on the selected square and return it.
+   * If the root is not a valid square array, print out the error message
+   *    to the console.
    * @param color user selected color
    * @param path the path of the target square
    * @param root the root of the square tree
    */
-  changeColor = (color: Color, path: Path | undefined, root:unknown ):unknown =>{
-    if (path === nil) {
+  changeColor = (color: Color,
+                 path: Path | undefined,
+                 root:unknown ):unknown =>{
+    if (path === nil) {  // base case
       return color;
     }
-    else {
-      if(Array.isArray(root) && root.length === 4 && path?.hd !== undefined) {
-        let data = [root[0], root[1], root[2], root[3]];
-        data[dirToIdx[path.hd]] = this.changeColor(color, path.tl, root[dirToIdx[path.hd]]);
-        return data;
+    else { // recursive cases
+      // Check if root is a valid array
+      if(Array.isArray(root) && root.length === 4) { // root is a valid array
+        // Check if a path has been selected
+        if (path === undefined) {  // No path selected yet
+          console.error(`No path selected yet`);
+          return root;
+        }
+        else {  // A path has been selected
+          // Assign the current root to the data array
+          const data:unknown[] = root;
+          // Change the color on the specified square by converting from dir to
+          // array index
+          data[dirToIdx[path.hd]] = this.changeColor(color, path.tl,
+            root[dirToIdx[path.hd]]);
+          return data;
+        }
       }
-      else {
+      else {  // root is not a valid array
         console.error(`type ${typeof root} is not a valid square`);
         return root;
       }
