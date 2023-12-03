0f3e16ad180b368b26618e8d5f724efe932b101b
Robert Tang <robertxt@cs.washington.edu>
2023-12-02 22:02:57 -0800

    hw9: add new routes to index.ts

--- hw/hw9-polls/server/src/index.ts
+++ hw/hw9-polls/server/src/index.ts
@@ -1,5 +1,5 @@
 import express, { Express } from "express";
-import { dummy } from './routes';
+import { list, save, load } from './routes';
 import bodyParser from 'body-parser';
 
 
@@ -7,5 +7,7 @@ import bodyParser from 'body-parser';
 const port: number = 8088;
 const app: Express = express();
 app.use(bodyParser.json());
-app.get("/api/dummy", dummy);  // TODO: REMOVE
+app.get("/api/list", list);
+app.post("/api/save", save);
+app.get("/api/load", load);
 app.listen(port, () => console.log(`Server listening on ${port}`));
