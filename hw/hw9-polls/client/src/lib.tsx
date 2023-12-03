a7c7b2c3db635216e4f47f3e184a54a6302c8ea6
Robert Tang <robertxt@cs.washington.edu>
2023-12-02 22:00:51 -0800

    hw9: add lib.tsx to project

---
+++ hw/hw9-polls/client/src/lib.tsx
@@ -0,0 +1,24 @@
+export type model = "polls" | "newPoll" | "vote" | "results";  // UI view
+export type action = undefined| "new" | "edit" | "save" | "refresh" | "back"; // action
+
+export type poll = {
+  name: string,
+  minutes: number,
+  options: string[],
+  createAt: Date
+};
+
+/**
+ * Return the difference between two dates in minutes
+ * @param src
+ * @param tar
+ */
+export const diffTimeFunc = (src: Date, tar: Date):number =>{
+  return (src.getTime() - tar.getTime()) / 1000 / 60
+}
+
+export const addMinutesFunc = (minutes: number, date: Date, ): Date => {
+  const newDate = new Date(date);
+  newDate.setMinutes(newDate.getMinutes() + minutes);
+  return newDate;
+}
