dc613751636f70034f5c7b52a3934717301a75a3
Robert Tang <robertxt@cs.washington.edu>
2023-12-03 22:10:58 -0800

    hw9: some changes to lib.tsx

--- hw/hw9-polls/client/src/lib.tsx
+++ hw/hw9-polls/client/src/lib.tsx
@@ -1,28 +1,37 @@
-export type model = "polls" | "newPoll" | "vote" | "results";  // UI view
-export type action = undefined| "new" | "edit" | "save" | "refresh" | "back"; // action
+// Type for components
+export type Comp = "polls" | "newPoll" | "vote" | "results";
 
-export type poll = {
+// Type for the poll
+export type Poll = {
   name: string,
   minutes: number,
   options: string[],
   createAt: Date
 };
 
-export type voteResult = {
-  poll: poll,
+// Type for the vote result
+export type VoteResult = {
+  poll: Poll,
   result: Map<string, number>,
   totalVotes: number
 }
 
+// Type for the server response
+export type ServerResponse = {msg: string}
+
 /**
- * Return the difference between two dates in minutes
- * @param src
- * @param tar
+ * Return the minute difference between two dates
+ * @param src The source date for comparing
+ * @param tar the target date for comparing
  */
 export const diffTimeFunc = (src: Date, tar: Date):number =>{
   return (src.getTime() - tar.getTime()) / 1000 / 60
 }
-
+/**
+ * Add minutes to a Date
+ * @param minutes The minutes in number
+ * @param date The target date to add minutes
+ */
 export const addMinutesFunc = (minutes: number, date: Date, ): Date => {
   const newDate = new Date(date);
   newDate.setMinutes(newDate.getMinutes() + minutes);
