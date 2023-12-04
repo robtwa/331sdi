dd4ad4a5c9a4fcd07e81a4bb0d72f629c012cfd6
Robert Tang <robertxt@cs.washington.edu>
2023-12-03 16:35:56 -0800

    hw9: add 3 test cases to routes_test.ts

--- hw/hw9-polls/server/src/routes_test.ts
+++ hw/hw9-polls/server/src/routes_test.ts
@@ -1,19 +1,83 @@
 import * as assert from 'assert';
 import * as httpMocks from 'node-mocks-http';
-import { dummy } from './routes';
+import {results, save, vote} from './routes';
 
 
 describe('routes', function() {
 
   // TODO: remove the tests for the dummy route
 
-  it('dummy', function() {
+  it('save', function() {
+    const payload = {
+      name: 'What do I eat for dinner?',
+      minute: 10,
+      options: "Dim Sum\nPizza\nPha",
+    };
     const req1 = httpMocks.createRequest(
-        {method: 'GET', url: '/api/dummy', query: {name: 'Bob'} });
+        {method: 'POST', url: '/api/save',
+          body: payload });
     const res1 = httpMocks.createResponse();
-    dummy(req1, res1);
+    save(req1, res1);
     assert.strictEqual(res1._getStatusCode(), 200);
-    assert.deepStrictEqual(res1._getData(), {msg: "Hi, Bob!"});
+    assert.deepStrictEqual(res1._getData(), {msg: `${payload.name} saved.`});
+  });
+
+  it('vote', function() {
+    const payload = {
+      name: 'What do I eat for dinner?',
+      voter: "voter1",
+      option: "Dim Sum",
+    };
+    const req1 = httpMocks.createRequest(
+      {method: 'POST', url: '/api/vote',
+        body: payload });
+    const res1 = httpMocks.createResponse();
+    vote(req1, res1);
+    console.log(res1._getData())
+    assert.strictEqual(res1._getStatusCode(), 200);
+    assert.deepStrictEqual(res1._getData(), {msg: `Recorded vote of "${payload.name}" as "${payload.option}"`});
+  });
+
+  it('result', function() {
+    // create a poll
+    const payload = {
+      name: 'What do I eat for dinner?',
+      minute: 10,
+      options: "Dim Sum\nPizza\nPha",
+    };
+    const req1 = httpMocks.createRequest(
+      {method: 'POST', url: '/api/save', body: payload });
+    const res1 = httpMocks.createResponse();
+    save(req1, res1);
+    assert.strictEqual(res1._getStatusCode(), 200);
+    assert.deepStrictEqual(res1._getData(), {msg: `${payload.name} saved.`});
+
+    // Make a vote
+    const payload2 = {
+      name: payload.name,
+      voter: "voter1",
+      option: "Dim Sum",
+    };
+    let req2 = httpMocks.createRequest(
+      {method: 'POST', url: '/api/vote',
+        body: payload2 });
+    let res2 = httpMocks.createResponse();
+    vote(req2, res2);
+    assert.strictEqual(res2._getStatusCode(), 200);
+    assert.deepStrictEqual(res2._getData(), {msg: `Recorded vote of "${payload2.name}" as "${payload2.option}"`});
+
+    // Get the voting result
+    const req3 = httpMocks.createRequest(
+      {method: 'GET', url: `/api/vote?name=${encodeURIComponent(payload.name)}` });
+    const res3 = httpMocks.createResponse();
+    results(req3, res3);
+    const data = JSON.parse(res3._getData());
+    console.log("data = ", data)
+    assert.strictEqual(res3._getStatusCode(), 200);
+    const result = new Map(data.result);
+    console.log("result = ", result)
+
+    // assert.deepStrictEqual(res3._getData(), {msg: `Recorded vote of "${payload.name}" as "${payload.option}"`});
   });
 
 });
