b621d8ea3a8527b62c2385c5bb50d639684c4c54
Robert Tang <robertxt@cs.washington.edu>
2023-12-03 16:30:34 -0800

    hw9: import two new routes to the index.ts

--- hw/hw9-polls/server/src/index.ts
+++ hw/hw9-polls/server/src/index.ts
@@ -1,5 +1,5 @@
 import express, { Express } from "express";
-import { list, save, load } from './routes';
+import {list, save, load, vote, results} from './routes';
 import bodyParser from 'body-parser';
 
 
@@ -10,4 +10,7 @@ app.use(bodyParser.json());
 app.get("/api/list", list);
 app.post("/api/save", save);
 app.get("/api/load", load);
+app.post("/api/vote", vote);
+app.get("/api/result", results);         // Get the data of a vote's result
+
 app.listen(port, () => console.log(`Server listening on ${port}`));
