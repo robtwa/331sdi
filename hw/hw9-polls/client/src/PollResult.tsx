fbc455f7add9669a0db979375ed297072d95b040
Robert Tang <robertxt@cs.washington.edu>
2023-12-05 14:38:38 -0800

    hw9: some changes to PollResult.tsx

--- hw/hw9-polls/client/src/PollResult.tsx
+++ hw/hw9-polls/client/src/PollResult.tsx
@@ -4,101 +4,110 @@ import {addMinutesFunc, diffTimeFunc, VoteResult} from "./lib";
 
 
 type VoteProps = {
-  backFunc:  () => void;
-  name: string;             // The name of the poll
+  backFunc:  () => void;          // The function that back to the poll list
+  name: string;                   // The name of the poll
 };
 
 type VoteState = {
-  dataLoaded: boolean;          // Indicates the state of data loading
-  name?: string;                // Poll name
-  minutes?: number;             // Voting duration
+  dataLoaded: boolean;            // Indicates the state of data loading
+  name?: string;                  // Poll name
+  minutes?: number;               // Voting duration
   options?: string[];             // Voting options
-  createdAt?:Date;             // Poll's created date and time
+  createdAt?:Date;                // Poll's created date and time
 
-  result?: Map<string, number>;
-  totalVotes?: number;
-  msg?: string | undefined;
+  result?: Map<string, number>;   // The result data
+  totalVotes?: number;            // How many votes
+  msg?: string | undefined;       // The message
 }
 
+// Displays the results of a poll.
 export class PollResult extends Component<VoteProps, VoteState> {
   constructor(props: VoteProps) {
     super(props);
     this.state = {dataLoaded:false};
   }
 
+  /**
+   * When a component is mounted
+   */
   componentDidMount = ():void => {
-    console.log("PollResult ///////////////////////////////////")
     this.doRefreshClick()
   }
 
-  // New Poll /////////////////////////////////////////////////////////////////
+  // Render the UI of the voting results
   render = (): JSX.Element | JSX.Element[] => {
-    console.log(this.state)
     if(!this.state.dataLoaded){  // Loading data from the server
       // In a commercial project, this can be an animation process
-      return <div>Data loading...</div>;
+      return <div key={"div_data_loading"} >Data loading...</div>;
     }
     else {  // Data is loaded from the server
       const retEles: JSX.Element[] = [];
-      if (this.state.createdAt === undefined) {
-        retEles.push(<>Corrupted data: Mission poll's creation time.</>);
-      }
-      else if (this.state.minutes === undefined) {
-        retEles.push(<>Corrupted data: Mission poll's minutes.</>);
+      if (this.state.minutes === undefined) {
+        retEles.push(<p key={"p_corrupted_data"} >
+          Corrupted data: Mission poll's minutes.</p>);
       }
       else if (this.state.options === undefined) {
-        retEles.push(<>Corrupted data: Mission poll's options.</>);
+        retEles.push(<p key={"p_corrupted_data"} >
+          Corrupted data: Mission poll's options.</p>);
+      }
+      else if (this.state.createdAt === undefined) {
+        retEles.push(<p key={"p_corrupted_data"} >
+          Corrupted data: Mission poll's creation time.</p>);
       }
       else {
         const end = addMinutesFunc(this.state.minutes, this.state.createdAt);
         const remainMinutes = diffTimeFunc(end, new Date());
 
-
         retEles.push(<h1 key="h1_vote">{this.state.name}</h1>);
 
-        if (remainMinutes > 0) {  // Poll is active
-          retEles.push(<p key="p_vote_active" >Closes in {Math.abs(remainMinutes).toFixed(2)} minutes</p>);
+        if (remainMinutes > 0) {  // Poll is open
+          retEles.push(<p key="p_vote_open" >Closes in {Math.abs(remainMinutes)
+            .toFixed(2)} minutes</p>);
         }
         else {  // Poll is closed
-          retEles.push(<p key="p_vote_closed">Closed {Math.abs(remainMinutes).toFixed(2)} minutes ago</p>);
+          retEles.push(<p key="p_vote_closed">Closed {Math.abs(remainMinutes)
+            .toFixed(2)} minutes ago</p>);
         }
 
         if (this.state.result!==undefined) {
           const list: JSX.Element[] = [];
           for (const [option, votes] of this.state.result) {
-            if (this.state.totalVotes !== undefined && this.state.totalVotes !== 0) {
-              list.push(<li>{Math.abs((votes / this.state.totalVotes) * 100).toFixed(0) }% - {option}</li>)
+            if (this.state.totalVotes !== undefined
+              && this.state.totalVotes !== 0) {
+              list.push(<li key={`li_option_${encodeURIComponent(option)}`}>
+                {Math.abs((votes / this.state.totalVotes) * 100)
+                  .toFixed(0) }% - {option}</li>)
             }
             else {
-              list.push(<li>0 - {option}</li>)
+              list.push(<li key={`li_option_${encodeURIComponent(option)}`}>
+                0% - {option}</li>)
             }
           }
-          retEles.push(<ul>{list}</ul>)
+          retEles.push(<ul key={"ul_voting_options"} >{list}</ul>)
         }
-
-        retEles.push(<p>{`Total number of voters: ${this.state.totalVotes}`}</p>)
-
-
-
+        retEles.push(<p key={"p_total_votes"}>
+          {`Total number of votes: ${this.state.totalVotes}`}</p>)
         retEles.push(<div key="div_btnarea_vote" className="buttonArea">
-          <button type="button" key="btn_back" onClick={this.props.backFunc} className="button">Back</button>
-          <button type="button" key="btn_refresh" onClick={this.doRefreshClick} className="button" >Refresh</button>
+          <button type="button" key="btn_back" onClick={this.props.backFunc}
+                  className="button">Back</button>
+          <button type="button" key="btn_refresh" onClick={this.doRefreshClick}
+                  className="button" >Refresh</button>
         </div>);
-
       }
       return retEles;
     }
-
   };
 
-  // Refresh //////////////////////////////////////////////////////////////////
-  // Send a request to the server to loads the poll data
+  /**
+   * Event handler for loading the voting result from the server
+   */
   doRefreshClick = ():void =>{
     fetch("/api/result?name="+encodeURIComponent(this.props.name))
       .then(this.doRefreshResp)
       .catch(()=>this.doRefreshError("failed to connect to server"));
   }
 
+  // Called when the server responds to the request.
   doRefreshResp = (res:Response):void => {
     if (res.status === 200) {
       res.json().then(this.doRefreshJson)
@@ -111,9 +120,8 @@ export class PollResult extends Component<VoteProps, VoteState> {
     }
   }
 
-  // Called when the save file response JSON has been parsed.
+  // Called when the JSON has been parsed.
   doRefreshJson = (data: VoteResult): void => {
-    console.log("data = ", data)
     this.setState({
       dataLoaded: true,
       name: data.poll.name,
@@ -121,12 +129,13 @@ export class PollResult extends Component<VoteProps, VoteState> {
       options: data.poll.options,
       createdAt: new Date(data.poll.createAt),
       result: new Map<string, number>(data.result),
-      totalVotes: data.totalVotes
+      totalVotes: data.totalVotes,
+      msg: "",
     })
   };
 
-  // Called if an error occurs trying to save file
+  // Called if an error occurs trying to get the voting result
   doRefreshError = (msg: string): void => {
-    this.setState({msg: `Error fetching /api/save: ${msg}`});
+    this.setState({msg: `Error fetching /api/result: ${msg}`});
   };
 }
